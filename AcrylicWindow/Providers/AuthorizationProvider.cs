d2d54006f1209420baa95c9f8321ae97cae2272d
TheWayToJunior <developermicrosoft1932@gmail.com>
2021-01-02 12:20:21 +0300

    Decomposed jwt parsing algorithm

--- AcrylicWindow/Providers/AuthorizationProvider.cs
+++ AcrylicWindow/Providers/AuthorizationProvider.cs
@@ -4,9 +4,7 @@ using AcrylicWindow.IContract.IProviders;
 using AcrylicWindow.Model;
 using IdentityModel.Client;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Security;
 using System.Security.Claims;
@@ -39,6 +37,7 @@ namespace AcrylicWindow.Providers
             }
 
             string token = JsonSerializer.Deserialize<UserSession>(File.ReadAllText(sessionPath)).Token;
+
             return BuildAuthenticationState(token);
         }
 
@@ -85,51 +84,7 @@ namespace AcrylicWindow.Providers
             _httpClient.SetBearerToken(token);
 
             return new AuthenticationState(
-                new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt")));
-        }
-
-        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
-        {
-            var claims = new List<Claim>();
-            var payload = jwt.Split('.')[1];
-            var jsonBytes = ParseBase64WithoutPadding(payload);
-            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
-
-            keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
-
-            if (roles != null)
-            {
-                if (roles.ToString().Trim().StartsWith("["))
-                {
-                    var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
-
-                    foreach (var parsedRole in parsedRoles)
-                    {
-                        claims.Add(new Claim(ClaimTypes.Role, parsedRole));
-                    }
-                }
-                else
-                {
-                    claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
-                }
-
-                keyValuePairs.Remove(ClaimTypes.Role);
-            }
-
-            claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
-
-            return claims;
-        }
-
-        private byte[] ParseBase64WithoutPadding(string base64)
-        {
-            switch (base64.Length % 4)
-            {
-                case 2: base64 += "=="; break;
-                case 3: base64 += "="; break;
-            }
-
-            return Convert.FromBase64String(base64);
+                new ClaimsPrincipal(new ClaimsIdentity(token.ParseClaimsFromJwt(), "jwt")));
         }
     }
 }
