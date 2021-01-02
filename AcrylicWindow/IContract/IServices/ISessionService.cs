c2e1595b1bd911319d7651a6e750c1288d2c794c
TheWayToJunior <developermicrosoft1932@gmail.com>
2021-01-02 13:58:11 +0300

    Made it possible to test AuthorizationProvider

---
+++ AcrylicWindow/IContract/IServices/ISessionService.cs
@@ -0,0 +1,13 @@
+﻿using AcrylicWindow.Model;
+using System.Threading.Tasks;
+
+namespace AcrylicWindow.IContract
+{
+    public interface ISessionService<T>
+        where T : UserSession
+    {
+        Task SaveAsync(string sessionPath, T session);
+
+        bool TryRecover(string sessionPath, out T result);
+    }
+}
